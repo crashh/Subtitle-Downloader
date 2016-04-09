5bc1c3a823e0837eaaa501843c0738b06bd4d80c
unknown <lthomasen laptop>
2016-04-09 18:05:43 +0200

    Made right textbar collapsable, reduced overall window size.

---
+++ SubtitleDownloaderV2/Converters/StringToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SubtitleDownloaderV2.Converters
+{
+    internal class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var toString = value as string;
+            return !string.IsNullOrEmpty(toString);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // If this is met, turn off TwoWay Binding.
+            return null;
+        }
+    }
+}
